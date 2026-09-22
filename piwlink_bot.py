52dead3c07800da8e6223a00b1b843e97c99bb1f
khan356-pixel <khantudeal@gmail.com>
2026-09-22 13:37:33 +0300

    Create piwlink_bot.py with supplier data
    Initialized the PIWLink bot with available suppliers.

---
+++ piwlink_bot.py
@@ -0,0 +1,20 @@
+# PIWLink - Water Marketplace
+
+suppliers = {
+    "Juba": [],
+    "Kampala": [],
+    "Gulu": [],
+    "Nairobi": [],
+    "Mombasa": [],
+    "Kisumu": [],
+    "Nakuru": [],
+    "Thika": [],
+    "Limuru": []
+}
+
+print("Welcome to PIWLink 💧")
+print("Connect with trusted water suppliers near you.")
+
+print("\nAvailable cities:")
+for city in suppliers:
+    print("-", city)
