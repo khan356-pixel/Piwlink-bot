bbc87435a8fa119895b34a66d230768874bfbe32
khan356-pixel <khantudeal@gmail.com>
2026-09-22 13:48:07 +0300

    Add water supplier information for multiple cities
    Added water supplier details for Juba, Kampala, Gulu, and Nairobi.

--- piwlink_bot.py
+++ piwlink_bot.py
@@ -1,15 +1,56 @@
 # PIWLink - Water Marketplace
 
 suppliers = {
-    "Juba": [],
-    "Kampala": [],
-    "Gulu": [],
-    "Nairobi": [],
+    "Juba": [
+        {
+            "name": "Juba Water Supplier 1",
+            "location": "Juba",
+            "phone": "000000000",
+            "price": 20000,
+            "available": 10000,
+            "delivery_fee": 5000
+        }
+    ],
+
+    "Kampala": [
+        {
+            "name": "Kampala Water Supplier 1",
+            "location": "Kampala",
+            "phone": "000000000",
+            "price": 20000,
+            "available": 10000,
+            "delivery_fee": 5000
+        }
+    ],
+
+    "Gulu": [
+        {
+            "name": "Gulu Water Supplier 1",
+            "location": "Gulu",
+            "phone": "000000000",
+            "price": 20000,
+            "available": 10000,
+            "delivery_fee": 5000
+        }
+    ],
+
+    "Nairobi": [
+        {
+            "name": "Nairobi Water Supplier 1",
+            "location": "Nairobi",
+            "phone": "000000000",
+            "price": 20000,
+            "available": 10000,
+            "delivery_fee": 5000
+        }
+    ],
+
     "Mombasa": [],
     "Kisumu": [],
     "Nakuru": [],
     "Thika": [],
     "Limuru": []
+}
 }
 
 print("Welcome to PIWLink 💧")
